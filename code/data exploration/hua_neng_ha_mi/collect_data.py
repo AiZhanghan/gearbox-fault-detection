90b4d147aebf471e3f3a4ef11281a6ca8b75663e
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-10-07 18:15:58 +0800

    1

---
+++ code/data exploration/hua_neng_ha_mi/collect_data.py
@@ -0,0 +1,139 @@
+"""
+处理原始csv文件
+"""
+
+
+import os
+import time
+import collections
+import pandas as pd
+
+
+def group_by_sensor(files):
+    """
+    根据传感器测点位置以及采样频率进行分组,
+
+    Args:
+        files: list[str]
+    
+    Return:
+        dict
+    """
+    # print("len(files): %d", len(files))
+    res = collections.defaultdict(list)
+    for file in files:
+        sensor = get_sensor_info(file)
+        res[sensor].append(file)
+    # 输出每个测点的数据数量
+    # for sensor in res:
+    #     print("%s: %d" % (sensor, len(res[sensor])))
+    return res
+
+
+def get_sensor_info(file):
+    """
+    Args:
+        file: str
+    
+    Return:
+        str
+    """
+    res = None
+    file_list = file.split("_")
+    if file_list[2] == "GEN":
+        res = "_".join(file_list[2: 6])
+    else:
+        res = "_".join(file_list[2: 5])
+    return res
+
+
+def filter_by_sensor(files, sensors):
+    """
+    过滤
+
+    Args:
+        files: dict
+        snesor: list[str]
+    
+    Return:
+        dict
+    """
+    res = {}
+    for key in files:
+        if key in sensors:
+            res[key] = files[key]
+    return res
+
+
+def collect_data(files, wind_turbine, source_path, target_path):
+    """
+    聚合成一个csv文件, 只要2018年之后的数据
+
+    Args:
+        files: list[str]
+        wind_turbine: str
+        source_path: str
+        target_path: str
+    """
+    dic = {
+        "Planet_径向_12800": "gearbox", 
+        "Shaft2_径向_25600": "low_speed_shaft", 
+        "Shaft3_径向_25600": "high_speed_shaft",
+    }
+    for sensor in files:
+        start = time.time()
+        print("%s: " % sensor, end="")
+        df = pd.DataFrame(files[sensor])
+        df.index = pd.to_datetime(df.apply(lambda x: x[0].split("_")[-1][: -4],
+            axis=1))
+        df.sort_index(inplace=True)
+        df = df["2018": ]
+
+        data = pd.DataFrame()
+        for path in df.values:
+            # np.array -> str
+            path = path[0]
+            temp = pd.read_csv(os.path.join(source_path, wind_turbine, path), 
+                header=None)
+            data = pd.concat([data, temp])
+        
+        data.index = df.index
+        data.dropna(axis=1, inplace=True)
+        
+        temp_path = os.path.join(target_path, wind_turbine)
+        if not os.path.exists(temp_path):
+            os.makedirs(temp_path)
+        data.to_csv(os.path.join(temp_path, dic[sensor]) + ".csv")
+        print("%.2f" % time.time() - start)
+
+
+def main():
+    source_path = r"E:\data\海装\CMS\华能三塘湖"
+    target_path = r"E:\data\海装\华能新疆哈密风电基地200MW风电工程项目"
+    # wind_turbines = os.listdir(source_path)
+    wind_turbines = [
+        "5",
+        # "29",
+        # "33",
+        # "40",
+        # "50",
+        # "58",
+        # "66",
+        # "73",
+        # "76",
+        # "86",
+        # "89",
+        # "96",
+    ]
+    
+    for wind_turbine in wind_turbines:
+        print("\nwind_turbine: %s" % wind_turbine)
+        files = os.listdir(os.path.join(source_path, wind_turbine))
+        files = group_by_sensor(files)
+        sensors = ["Planet_径向_12800", "Shaft2_径向_25600", "Shaft3_径向_25600"]
+        files = filter_by_sensor(files, sensors)
+        collect_data(files, wind_turbine, source_path, target_path)
+
+
+if __name__ == "__main__":
+    main()
