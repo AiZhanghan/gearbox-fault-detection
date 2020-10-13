5431350938dd9a8ded81961d4b2c66916de14ce2
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-10-13 16:36:30 +0800

    1

--- code/model_entry.py
+++ code/model_entry.py
@@ -3,7 +3,9 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from pyod.models.auto_encoder import AutoEncoder
+from sklearn.decomposition import PCA
+# from pyod.models.auto_encoder import AutoEncoder
+from pyod.models.knn import KNN
 from pyod.models.combination import average
 from pyod.utils.utility import standardizer
 
@@ -14,17 +16,31 @@ class OutlierDetector:
     """
     Attr:
         decision_scores: pd.DataFrame, 训练集异常值
-        threshold: float, 阈值
         label: pd.DataFrame, 训练集预测标签
+        threshold: float, 阈值
     
     Instance API:
-        fit(X): 拟合模型
+        fit(X, contamination): 拟合模型
         decision_function(X): 使用拟合好的模型预测
-        load(path)
+        predict(X)
+        # load(path)
 
     Static API:
-        save(path)
+        # save(path)
     """
+    def __init__(self):
+        self.decision_scores = None
+        self.label = None
+        self.threshold = None
+        # 模型
+        self.detectors = None
+        # data scaler
+        self.data_norm_scalar = None
+        self.data_unif_scalar = None
+        # reducer
+        self.reducer = None
+        # score scaler
+        self.score_scalar = None
 
     def fit(self, X, contamination=0.01):
         """
@@ -34,29 +50,26 @@ class OutlierDetector:
             X: pd.DataFrame
         """
         self.detectors = {
-            "auto_encoder": AutoEncoder(
-                epochs=256,
-                validation_size=0,
-                preprocessing=False,
-                verbose=0,
-                contamination=contamination,
-            ),
+            "knn": KNN(contamination=contamination),
+            # "auto_encoder": AutoEncoder(
+            #     epochs=256,
+            #     validation_size=0,
+            #     preprocessing=False,
+            #     verbose=0,
+            #     contamination=contamination,
+            # ),
         }
-        # print("train_data.shape:", X.shape)
         # 数据预处理
-        # 标准化
-        X_train_norm, self.data_norm_scalar = standardizer(X, 
-            keep_scalar=True)
-        # 归一化
-        X_train_unif, self.data_unif_scalar = minmaxizer(X_train_norm, 
-            keep_scalar=True)
-        
+        X_train = self.data_preprocess_fit_transform(X)
+        # 降维
+        X_train = self.reduction_fit_transform(X_train)
+        # scores thresholds初始化
         train_scores = np.zeros([X.shape[0], len(self.detectors)])
         thresholds = np.zeros([1, len(self.detectors)])
         # 训练
         for i, clf_name in enumerate(self.detectors):
             clf = self.detectors[clf_name]
-            clf.fit(X_train_unif)
+            clf.fit(X_train)
             train_scores[:, i] = clf.decision_scores_
             thresholds[:, i] = clf.threshold_
         # 训练集异常程度及阈值
@@ -81,13 +94,13 @@ class OutlierDetector:
             anomaly_scores: pd.DataFrame
         """
         # 数据预处理
-        X_test_norm = self.data_norm_scalar.transform(X)
-        X_test_unif = self.data_unif_scalar.transform(X_test_norm)
-
-        test_scores = np.zeros([X_test_unif.shape[0], len(self.detectors)])
+        X_test = self.data_preprocess_transform(X)
+        # 降维
+        X_test = self.reduction_transform(X_test)
+        test_scores = np.zeros([X_test.shape[0], len(self.detectors)])
         for i, clf_name in enumerate(self.detectors):
             test_scores[:, i] = self.detectors[clf_name].\
-                decision_function(X_test_unif)
+                decision_function(X_test)
         
         test_scores_norm = self.score_scalar.transform(test_scores)
         anomaly_scores = pd.DataFrame(average(test_scores_norm), 
@@ -108,6 +121,56 @@ class OutlierDetector:
         anomaly_scores = self.decision_function(X)
         # 连续三次超过阈值
         return self.get_label(anomaly_scores)
+    
+    def data_preprocess_fit_transform(self, X):
+        """
+        Args:
+            X: pd.DataFrame
+        
+        Return:
+            np.array
+        """
+        # 数据预处理
+        # 标准化
+        X_train_norm, self.data_norm_scalar = standardizer(X, 
+            keep_scalar=True)
+        # 归一化
+        X_train_unif, self.data_unif_scalar = minmaxizer(X_train_norm, 
+            keep_scalar=True)
+        return X_train_unif
+    
+    def data_preprocess_transform(self, X):
+        """
+        Args:
+            X: pd.DataFrame
+        
+        Return:
+            np.array
+        """
+        X_test_norm = self.data_norm_scalar.transform(X)   
+        X_test_unif = self.data_unif_scalar.transform(X_test_norm)
+        return X_test_unif
+
+    def reduction_fit_transform(self, X):
+        """
+        Args:
+            X: np.array
+        
+        Return:
+            np.array
+        """
+        self.reducer = PCA(n_components=8)
+        return self.reducer.fit_transform(X)
+
+    def reduction_transform(self, X):
+        """
+        Args:
+            X: np.array
+        
+        Return:
+            np.array
+        """
+        return self.reducer.transform(X)
     
     def get_label(self, anomaly_scores):
         """
